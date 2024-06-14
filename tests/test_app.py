6aa314a00795e8c7bf87a60efed7fb4b30df24f3
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2024-06-13 23:24:38 -0300

    configurando e iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (organização)
+    response = client.get('/')  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # assert (validação)
+    assert response.json() == {'message': 'Ola mundo'}
